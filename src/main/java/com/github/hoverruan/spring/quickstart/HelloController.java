84db65c8f624507c1f5cfc0eec014684d6c645d6
Hover Ruan <hoverruan@gmail.com>
2013-04-11 18:07:15 +0800

    change scan base package

--- src/main/java/com/github/hoverruan/spring/quickstart/HelloController.java
+++ src/main/java/com/github/hoverruan/spring/quickstart/HelloController.java
@@ -1,22 +1,14 @@
-package com.github.wayne1986.spring;
+package com.github.hoverruan.spring.quickstart;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Created with IntelliJ IDEA.
- * User: huhuanfeng
- * Date: 13-4-11
- * Time: 下午3:22
- * To change this template use File | Settings | File Templates.
- */
 @Controller
 public class HelloController {
     @RequestMapping("/hello")
     @ResponseBody
     public String Hello() {
         return "Hello !";
-
     }
 }
